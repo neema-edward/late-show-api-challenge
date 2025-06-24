59ccc4d24aec1f677ea430cbe67ae3dd8e1b9551
Neema <neema.edward@student.moringaschool.com>
2025-06-24 11:53:45 +0300

    updates the init file for the controllers

---
+++ server/controllers/__init__.py
@@ -0,0 +1,4 @@
+from .auth_controller import register, login
+from .guest_controller import get_guests
+from .episode_controller import get_episodes, get_episode, delete_episode
+from .appearance_controller import create_appearance
